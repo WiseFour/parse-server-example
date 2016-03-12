3d1f8c70940c911397cc38b2859dfc8d185f101c
Seongjin Cho <sjcho@wisefour.com>
2016-03-12 21:12:54 +0900

    Adds more config settings for sending email

--- index.js
+++ index.js
@@ -11,12 +11,14 @@ if (!databaseUri) {
 }
 
 var emailAdapter = null;
-if (process.env.MAILGUN_API_KEY && process.env.MAILGUN_DOMAIN) {
+if (process.env.MAILGUN_API_KEY && process.env.MAILGUN_DOMAIN &&
+    process.env.MAILGUN_FROM_ADDRESS) {
   emailAdapter = {
     module: './Email/SimpleMailgunAdapter',
     options: {
       apiKey: process.env.MAILGUN_API_KEY,
       domain: process.env.MAILGUN_DOMAIN,
+      fromAddress: process.env.MAILGUN_FROM_ADDRESS,
     },
   };
 }
@@ -25,8 +27,10 @@ var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
+  appName: process.env.APP_NAME || 'myApp',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337',  // Don't forget to change to https if needed
+  publicServerURL: process.env.PUBLIC_SERVER_URL || 'http://localhost:1337',
   emailAdapter: emailAdapter,
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
